f73b8db0adedf81e1f312a948bc74b6fa54dd126
Ultrasolutions2024 <ghultraflysolutions@gmail.com>
2024-11-07 10:36:20 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
   //   config.resolve.alias['@'] = path.resolve('src');
   //   return config;
   // },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   output: 'export', // Ensures the build creates a static site.
   trailingSlash: true, // Optional: Ensures proper linking for static pages.
   images: {
