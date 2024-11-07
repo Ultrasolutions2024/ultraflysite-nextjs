e64a42f09ad99f8dbb6f8f6a4d795d333954756d
Ultrasolutions2024 <ghultraflysolutions@gmail.com>
2024-11-07 14:45:11 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
   output: 'export', // Ensures the build creates a static site.
   trailingSlash: true, // Optional: Ensures proper linking for static pages.
   images: {
-    domains: ['readymadeui.com',"https://ultraflysolutions.com/"], // Add the external domain here.
+    domains: ['readymadeui.com', 'ultraflysolutions.com'], // Add the external domain here.
     unoptimized:true
   },
 };
